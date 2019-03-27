7ddb1cffc2a158a6023a8ffd3e32473e4b4f9110
Michell Tejada <michelltejada@pursuit.com>
2019-03-27 15:48:11 -0400

    Added Route in app

--- healthy_jungle/src/App.js
+++ healthy_jungle/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Home from './Components/Homepage/Home';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App" >
-        <Home /> 
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
       </div>
     );
 
