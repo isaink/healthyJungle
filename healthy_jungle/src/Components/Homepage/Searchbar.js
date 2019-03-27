bcea5cc9ecfb59f3151f766de0f4cb7e33c5e9a5
Michell Tejada <michelltejada@pursuit.com>
2019-03-27 14:09:42 -0400

    Searchbar form made

--- healthy_jungle/src/Components/Homepage/Searchbar.js
+++ healthy_jungle/src/Components/Homepage/Searchbar.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react'
 
 class Searchbar extends Component {
 
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+  }
+
   render(){
     return (
       <div>
-        
+        <form onSubmit = {this.handleSubmit}>
+          <input
+            type='text'
+            onChange={this.props.handleChange}
+            placeholder='Search for recipe'
+            value={this.props.searchInput}
+          />
+          <button type='submit'>Submit</button>
+        </form>
       </div>
     )
   }
 }
-
-
-
 
 
 export default Searchbar
