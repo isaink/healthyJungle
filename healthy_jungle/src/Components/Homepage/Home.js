5e06710bbad445dbc3d96a2fc34c531d03b14cc2
baynejd <jonathanbayne@pursuit.org>
2019-03-27 15:32:52 -0400

    committing

--- healthy_jungle/src/Components/Homepage/Home.js
+++ healthy_jungle/src/Components/Homepage/Home.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router'
-import axios from 'axios'
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import axios from 'axios';
+import '../../secrets.js'
 
 
 class Home extends Component {
@@ -11,11 +12,6 @@ class Home extends Component {
     };
   };
 
-
-  let url
-
-  axios.get(`"https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
-  `)
   render(){
 
     return(
